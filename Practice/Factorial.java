5542ba64c88fb53ce84fd8d739e8b0e799e7fc38
Ayush Bansal <pk3535745@gmail.com>
2025-03-23 15:45:19 +0530

    Commit 1

--- Practice/Factorial.java
+++ Practice/Factorial.java
@@ -5,8 +5,8 @@ public class Factorial {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         int factorial = 1;
-        for (int i = 1; i < n; i++) {
-            factorial *= i;
+        for (int i = 1; i <= n; i++) {
+            factorial = factorial * i;
         }
         System.out.println("Factorial: "+factorial);
         sc.close();
